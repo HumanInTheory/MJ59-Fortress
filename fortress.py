64b1ae4f4f9ad410e115d1bf6257be4251e568aa
HumanInTheory <ahumanintheory@gmail.com>
2020-07-26 15:18:15 -0700

    Added credits and a state machine to handle inputs and swapping scenes.
    Signed-off-by: HumanInTheory <ahumanintheory@gmail.com>

--- fortress.py
+++ fortress.py
@@ -28,7 +28,16 @@ class Map:
     def getbuffer(self):
         return self.buffer
 
-    def drawWall(self, thick, x, y, w, h):
+    def fillchar(self, char):
+        self.buffer["ch"][:,:] = char
+
+    def fillfg(self, color):
+        self.buffer["fg"][:,:] = color
+
+    def fillbg(self, color):
+        self.buffer["bg"][:,:] = color
+
+    def draw_wall(self, thick, x, y, w, h):
         for i in range(x, x+w):
             for j in range(y, y+h):
                 if (i==x):
@@ -54,7 +63,7 @@ class Map:
                         char = ' '
                 self.setchar(ord(char), i, j)
     
-    def printStr(self, x, y, str, vert: bool=False):
+    def print_str(self, x, y, str, vert: bool=False):
         if (vert):
             for i in range(len(str)):
                 self.setchar(ord(str[i]), x, y+i)
@@ -62,81 +71,140 @@ class Map:
             for i in range(len(str)):
                 self.setchar(ord(str[i]), x+i, y)
 
-menuchar = ["╔╦╦╦╦╦╦╦╦╦╦╦╦╦╦╗",
-            "╠              ╣",
-            "╠   FORTRESS   ╣",
-            "╠              ╣",
-            "╠   ╔╦╦╦╦╦╦╗   ╣",
-            "╠   ╠      ╣   ╣",
-            "╠   ╠ PLAY ╣   ╣",
-            "╠   ╠      ╣   ╣",
-            "╠   ╠      ╣   ╣",
-            "╠   ╠CREDIT╣   ╣",
-            "╠   ╠      ╣   ╣",
-            "╠   ╚╩╩╩╩╩╩╝   ╣",
-            "╠              ╣",
-            "╠              ╣",
-            "╠              ╣",
-            "╚╩╩╩╩╩╩╩╩╩╩╩╩╩╩╝"]
+class Selector:
+    def __init__(self, x, y, w, h):
+        self.selected = False
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.map = Map(h, w)
+        self.selfg = (0, 255, 0, 255)
+        self.selbg = (64, 128, 64, 255)
+        self.unselfg = (255, 255, 255, 255)
+        self.unselbg = (0, 0, 0, 255)
+    
+    def setselected(self, set):
+        if (set):
+            self.map.fillfg(self.selfg)
+            self.map.fillbg(self.selbg)
+        else:
+            self.map.fillfg(self.unselfg)
+            self.map.fillbg(self.unselbg)
 
-menufg = ["                ",
-          "                ",
-          "    rrrrrrrr    ",
-          "                ",
-          "                ",
-          "                ",
-          "      gggg      ",
-          "                ",
-          "                ",
-          "     bbbbbb     ",
-          "                ",
-          "                ",
-          "                ",
-          "                ",
-          "                ",
-          "                "]
+    def draw_sel(self, cnsl):
+        for i in range(0,self.w):
+            for j in range(0,self.h):
+                cnsl.tiles[self.y+j,self.x+i] = self.map.buffer[j,i]
 
-menubg = ["                ",
-          "                ",
-          "                ",
-          "                ",
-          "                ",
-          "                ",
-          "                ",
-          "                ",
-          "                ",
-          "                ",
-          "                ",
-          "                ",
-          "                ",
-          "                ",
-          "                ",
-          "                "]
+class UI:
+    def __init__(self):
+        self.cells = []
+        self.currentcell = 0
 
+    def addcell(self, cell):
+        self.cells.append(cell)
+        self.cells[0].setselected(True)
+
+    def process_input(self, input):
+        self.cells[self.currentcell].setselected(False)
+        if (input):
+            self.currentcell += 1
+        else:
+            self.currentcell -= 1
+        self.currentcell = self.currentcell%len(self.cells)
+        self.cells[self.currentcell].setselected(True)
+
+    def draw_ui(self, cnsl):
+        for i in range(len(self.cells)):
+            self.cells[i].draw_sel(cnsl)
+
+class Scene():
+    def __init__(self):
+        self.map = Map(WIDTH, HEIGHT)
+        self.ui = UI()
+
+    def process_input(self, event):
+        return ""
+
+    def draw(self, cnsl):
+        for i in range(WIDTH):
+            for j in range(HEIGHT):
+                cnsl.tiles[i, j] = self.map.buffer[i, j]
+                self.ui.draw_ui(cnsl)
+
+class GameState():
+    def __init__(self, initialscene):
+        self.currentscene = initialscene
+
+    def set_scene(self, newscene):
+        self.currentscene = newscene
+
+# Main Menu State
+# Make the map
 menu = Map(16, 16)
-menu.setfg((255,255,255,255),0,0)
-menu.drawWall(True,0,0,16,16)
-menu.drawWall(True,0,0,4,4)
-menu.drawWall(True,12,0,4,4)
-menu.drawWall(True,0,12,4,4)
-menu.drawWall(True,12,12,4,4)
-menu.drawWall(False,4,4,8,8)
-menu.printStr(4,2,"FORTRESS")
-menu.printStr(6,6,"PLAY")
-menu.printStr(5,9,"CREDIT")
+menu.draw_wall(True,0,0,16,16)
+menu.draw_wall(True,0,0,4,4)
+menu.draw_wall(True,12,0,4,4)
+menu.draw_wall(True,0,12,4,4)
+menu.draw_wall(True,12,12,4,4)
+menu.print_str(4,2,"FORTRESS")
 
-class Room:
-    """A simple display class."""
-    def __init__(self, name, map, p_loc, disp_player):
-        self.name = name
-        self.map = map
-        self.p_loc = p_loc
-        self.disp_player = disp_player
+# Make the ui
+menu_ui = UI()
+play = Selector(6,6,4,1)
+play.map.print_str(0,0,"PLAY")
+menu_ui.addcell(play)
+credit = Selector(5,9,6,1)
+credit.map.print_str(0,0,"CREDIT")
+menu_ui.addcell(credit)
 
-def draw(cnsl):
-    for i in range(WIDTH):
-        for j in range(HEIGHT):
-            cnsl.tiles[i, j] = menu.buffer[i, j]
+# Declare the state
+class MainMenu(Scene):
+    def __init__(self):
+        self.map = menu
+        self.ui = menu_ui
+
+    def process_input(self, event):
+        if event.type == "KEYDOWN":
+            if event.scancode == tcod.event.SCANCODE_UP:
+                menu_ui.process_input(True)
+            elif event.scancode == tcod.event.SCANCODE_DOWN:
+                menu_ui.process_input(False)
+            elif event.scancode == tcod.event.SCANCODE_RETURN:
+                if menu_ui.currentcell == 0:
+                    return "play"
+                else:
+                    return "credits"
+        return ""
+
+# Make the credits state
+# Make the map
+credits = Map(16, 16)
+credits.draw_wall(True,0,0,16,16)
+credits.draw_wall(True,0,0,4,4)
+credits.draw_wall(True,12,0,4,4)
+credits.draw_wall(True,0,12,4,4)
+credits.draw_wall(True,12,12,4,4)
+credits.print_str(4,2,"FORTRESS")
+credits.print_str(2,5,"HUMANINTHEORY")
+credits.print_str(2,7,"ASSETS:KENNY")
+credits.print_str(3,9,"MADE USING")
+credits.print_str(4,10,"LIBTCOD")
+credits.print_str(5,11,"NUMPY")
+# No ui
+empty_ui = UI()
+
+class Credits(Scene):
+    def __init__(self):
+        self.map = credits
+        self.ui = empty_ui
+    
+    def process_input(self, event):
+        if event.type == "KEYDOWN":
+            if event.scancode == tcod.event.SCANCODE_ESCAPE:
+                return "main"
+        return ""
 
 def main() -> None:
     """Script entry point."""
@@ -146,18 +214,25 @@ def main() -> None:
     )
     # Create the main console.
     console = tcod.Console(WIDTH, HEIGHT)
+    mainmenu_s = MainMenu()
+    credits_s = Credits()
+    statemachine = GameState(credits_s)
     # Create a window based on this console and tileset.
     with tcod.context.new_terminal(
         console.width, console.height, tileset=tileset,
     ) as context:
         while True:  # Main loop, runs until SystemExit is raised.
             console.clear()
-            draw(console)
+            statemachine.currentscene.draw(console)
             context.present(console, keep_aspect=True, clear_color=(0,0,0))  # Show the console.
-
             for event in tcod.event.wait():
                 context.convert_event(event)  # Sets tile coordinates for mouse events.
                 print(event)  # Print event information to stdout.
+                newstate = statemachine.currentscene.process_input(event)
+                if newstate == "main":
+                    statemachine.set_scene(mainmenu_s)
+                elif newstate == "credits":
+                    statemachine.set_scene(credits_s)
                 if event.type == "QUIT":
                     raise SystemExit()
         # The window will be closed after the above with-block exits.
